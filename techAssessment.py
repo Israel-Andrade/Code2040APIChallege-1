deabe76a61c066a2f72d32650d72e72f1b8d36a9
Israel Andrade <israelandrade18957@student.hartnell.edu>
2016-09-07 10:03:53 -0700

    Complete step 1

--- techAssessment.py
+++ techAssessment.py
@@ -1,22 +1,22 @@
-"""The modules used were requests to access the post method"""
 import requests
-"""The json module to use json dictionary"""
 import json
-from requests.utils import quote
+import math
+import datetime
+import time
 
-"""The registration endpoint for our API"""
+#The registration endpoint for our API
 url = 'http://challenge.code2040.org/api/register'
+token = '03cbcbfd47031937a28ff8bb5eccb71a'
+github ='https://github.com/IsraelAndrade22/Code2040APIChallege'
 
-"""Our dictionary entitled payload with two keys. Our token which hold the value for our api t
-	oken and github key which holds our github URL"""
+def apiRegistration(url_endpoint, token, github_url):
+	"""Request post method to register to our endpoint. The medthod will have two parameteres,
+	our url for the registration endpoint and json dictionary"""
 
-payload = {
-	'token': '03cbcbfd47031937a28ff8bb5eccb71a',
-	'github': 'https://github.com/IsraelAndrade22/Code2040APIChallenge'
-}
-
-"""Request post method to register to our endpoint. The medthod will have two parameteres,
-   our url for the registration endpoint and json dictionary"""
-
-r = requests.post(url, data = payload)
-print(r.text)
+	payload = {
+	'token': token,
+	'github': github_url
+	}
+	r = requests.post(url_endpoint, data = payload)
+	print(r.text)
+apiRegistration(url, token, github)
