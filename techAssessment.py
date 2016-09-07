bcf2de8239f430b5ab4bcf8d5bf1968696ff1dca
Israel Andrade <israelandrade18957@student.hartnell.edu>
2016-09-07 10:05:26 -0700

    Complete step 2

--- techAssessment.py
+++ techAssessment.py
@@ -20,3 +20,33 @@ def apiRegistration(url_endpoint, token, github_url):
 	r = requests.post(url_endpoint, data = payload)
 	print(r.text)
 apiRegistration(url, token, github)
+
+
+#The Endpoint to our next challenge 
+url_reverse = 'http://challenge.code2040.org/api/reverse'
+#The URL to post our JSON object
+url_validate = 'http://challenge.code2040.org/api/reverse/validate'
+
+def reverseString(url_reverse, url_validate, token):
+	"""Obtain a string value from our url_reverse endpoint and 
+	return the string reversed to the url_validate"""
+	#Dictionary with the key being our token
+	payload = {
+		'token': token
+	}
+	#Post method to the endpoint which will return a string 
+	r = requests.post(url_reverse, data = payload)
+	#Work will be converted to a string returned by the post method
+	word = str(r.content)
+	#Reverse the string word
+	reversed_word = word[::-1]
+	#Dictionary that will be used in our post method
+	#The key values will be the token for our challenge
+	#And the string word reversed
+	payload = {
+		'token': token,
+		'string': reversed_word
+	}
+	r = requests.post(url_validate, data = payload)
+	print(r.content)
+reverseString(url_reverse, url_validate, token)
